1bb8037e097e5c8cecb431f91707911ae45eb757
Nicolas Neubauer <nicolas.neubauer@gmail.com>
2013-03-13 22:39:08 +0100

    trying to get stuff to work

--- exampleapp.py
+++ exampleapp.py
@@ -19,9 +19,71 @@ app_url = 'https://graph.facebook.com/{0}'.format(FB_APP_ID)
 FB_APP_NAME = json.loads(requests.get(app_url).content).get('name')
 FB_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
 
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_object('conf.Config')
+
+
+def get_home():
+    return 'https://' + request.host + '/'
+
+
+def get_token():
+
+    if request.args.get('code', None):
+        return fbapi_auth(request.args.get('code'))[0]
+
+    cookie_key = 'fbsr_{0}'.format(FB_APP_ID)
+
+    if cookie_key in request.cookies:
+
+        c = request.cookies.get(cookie_key)
+        encoded_data = c.split('.', 2)
+
+        sig = encoded_data[0]
+        data = json.loads(urlsafe_b64decode(str(encoded_data[1]) +
+            (64-len(encoded_data[1])%64)*"="))
+
+        if not data['algorithm'].upper() == 'HMAC-SHA256':
+            raise ValueError('unknown algorithm {0}'.format(data['algorithm']))
+
+        h = hmac.new(FB_APP_SECRET, digestmod=hashlib.sha256)
+        h.update(encoded_data[1])
+        expected_sig = urlsafe_b64encode(h.digest()).replace('=', '')
+
+        if sig != expected_sig:
+            raise ValueError('bad signature')
+
+        code =  data['code']
+
+        params = {
+            'client_id': FB_APP_ID,
+            'client_secret': FB_APP_SECRET,
+            'redirect_uri': '',
+            'code': data['code']
+        }
+
+        from urlparse import parse_qs
+        r = requests.get('https://graph.facebook.com/oauth/access_token', params=params)
+        token = parse_qs(r.content).get('access_token')
+
+        return token
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/channel.html', methods=['GET', 'POST'])
+def get_channel():
+    return render_template('channel.html')
+
+
+@app.route('/close/', methods=['GET', 'POST'])
+def close():
+    return render_template('close.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
